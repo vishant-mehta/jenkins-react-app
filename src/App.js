12f07b9f9ffec1077895befec3654bfdb8b64ddc
Nishavak <44199073+nishavak@users.noreply.github.com>
2021-11-08 19:26:43 +0530

    Create react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Devops IA2</p>
+      </div>
+    )
+  }
+}
